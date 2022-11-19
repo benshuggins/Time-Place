b47eddd80112b2950c7a8288fb8ef29cc89565f7
Ben Huggins <benshuggins@gmail.com>
2022-11-19 11:04:49 -0800

    Initial Commit

---
+++ GymTracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GymTracker
+//
+//  Created by Ben Huggins on 11/19/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
